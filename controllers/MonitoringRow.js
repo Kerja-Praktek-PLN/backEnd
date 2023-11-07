8ae0e16efa7af522e3c82c52691d3cbc17405276
luckyboyyy09 <102453829+Luckyboyyy09@users.noreply.github.com>
2023-11-07 17:08:16 +0800

    new validationError system for createRow

--- controllers/MonitoringRow.js
+++ controllers/MonitoringRow.js
@@ -27,24 +27,21 @@ export const createRow = async (req, res) => {
   try {
     const { nomor_tower, tanggal, nama_PIC, jumlah_tegakan, status_tegakan, jenis_pohon, jalur, jarak_pohon_ke_konduktor, tindak_lanjut } = req.body;
 
-    // if (nomor_tower instanceof Number) {
-    //   nomor_tower = Number(nomor_tower);
-    //   return res.status(400).json({ msg: "nomor_tower must be a number" });
-    // }
+    if (isNaN(nomor_tower)) {
+      return res.status(400).json({ msg: "nomor_tower must be a number" });
+    }
 
-    // if (nomor_tower < 0) {
-    //   return res.status(400).json({ msg: "nomor_tower must be positive" });
-    // }
+    if (nomor_tower < 0) {
+      return res.status(400).json({ msg: "nomor_tower must be positive" });
+    }
 
-    // if (jumlah_tegakan instanceof Number) {
-    //   jumlah_tegakan = Number(jumlah_tegakan);
-    //   return res.status(400).json({ msg: "nomor_tower must be a number" });
-    // }
+    if (isNaN(jumlah_tegakan)) {
+      return res.status(400).json({ msg: "jumlah tegakan must be a number" });
+    }
 
-    // if (jarak_pohon_ke_konduktor instanceof Number) {
-    //   jarak_pohon_ke_konduktor = Number(jarak_pohon_ke_konduktor);
-    //   return res.status(400).json({ msg: "nomor_tower must be a number" });
-    // }
+    if (isNaN(jarak_pohon_ke_konduktor)) {
+      return res.status(400).json({ msg: "jarak pohon ke konduktor must be a number" });
+    }
 
     await Row.create(req.body);
     res.status(201).json({ msg: "Row Created" });
