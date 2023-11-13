0da15ab6ae44170d512ef0d9fc5c238f8e65bf0f
nrslmyyy <nurislamiyah370@gmail.com>
2023-11-13 13:48:55 +0800

    be ubah auth

--- controllers/MonitoringRow.js
+++ controllers/MonitoringRow.js
@@ -1,4 +1,3 @@
-
 import Row from "../models/MonitoringRow.js";
 
 export const getRow = async (req, res) => {
@@ -75,87 +74,6 @@ export const deleteRow = async (req, res) => {
     res.status(200).json({ msg: "Row deleted" });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ msg: "Internal server error" });
-  }
+    res.status(500).json({ msg: "Internal server error" });
+  }
 };
-
-import Row from "../models/MonitoringRow.js";
-
-export const getRow = async (req, res) => {
-  try {
-    const response = await Row.findAll();
-    res.status(200).json(response);
-  } catch (error) {
-    console.log(error.message);
-  }
-};
-
-export const getRowById = async (req, res) => {
-  try {
-    const response = await Row.findOne({
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.status(200).json(response);
-  } catch (error) {
-    console.log(error.message);
-  }
-};
-
-export const createRow = async (req, res) => {
-  try {
-    const { nomor_tower, jumlah_tegakan, jarak_pohon_ke_konduktor } = req.body;
-
-    if (isNaN(nomor_tower)) {
-      return res.status(400).json({ msg: "nomor_tower must be a number" });
-    }
-
-    if (nomor_tower < 0) {
-      return res.status(400).json({ msg: "nomor_tower must be positive" });
-    }
-
-    if (isNaN(jumlah_tegakan)) {
-      return res.status(400).json({ msg: "jumlah tegakan must be a number" });
-    }
-
-    if (isNaN(jarak_pohon_ke_konduktor)) {
-      return res.status(400).json({ msg: "jarak pohon ke konduktor must be a number" });
-    }
-
-    await Row.create(req.body);
-    res.status(201).json({ msg: "Row Created" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ msg: "Internal server error" });
-  }
-};
-
-export const updateRow = async (req, res) => {
-  try {
-    await Row.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.status(200).json({ msg: "Row updated" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ msg: "Internal server error" });
-  }
-};
-
-export const deleteRow = async (req, res) => {
-  try {
-    await Row.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.status(200).json({ msg: "Row deleted" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ msg: "Internal server error" });
-  }
-};
->>>>>>> origin/main
