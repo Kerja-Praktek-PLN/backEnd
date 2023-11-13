76b9b00ecdc0f75675b78f3fc31b0c22ea23cce3
luckyboyyy09 <102453829+Luckyboyyy09@users.noreply.github.com>
2023-11-13 12:57:08 +0800

    new update for berita acara table

--- models/BeritaAcara.js
+++ models/BeritaAcara.js
@@ -8,6 +8,11 @@ const { DataTypes } = Sequelize;
 const Berita_Acara = db.define(
   "Berita_Acara",
   {
+    id_acara: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     nama_PIC: {
       type: DataTypes.STRING,
     },
