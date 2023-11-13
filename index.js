78bdb0db43768277fab539ba87ed00125709d07e
luckyboyyy09 <102453829+Luckyboyyy09@users.noreply.github.com>
2023-11-13 12:59:03 +0800

    new import fileUpload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express";
+import fileUpload from "express-fileupload";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import db from "./config/Database.js";
-import Users from "./models/UserModel.js";
 import authRoutes from "./routes/Authentication.js";
 import router from "./routes/MonitoringRow.js";
 dotenv.config();
@@ -12,13 +12,14 @@ const app = express();
 try {
   await db.authenticate();
   console.log("Database Connceted..");
-  await Users.sync();
 } catch (error) {
   console.log(error);
 }
 
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieParser());
+app.use(fileUpload());
+app.use(express.static("public"));
 app.use(express.json());
 app.use(authRoutes);
 app.use(router);
