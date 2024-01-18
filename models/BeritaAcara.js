541aab4f5381aed4c7d4d19db98898542c063735
luckyboyyy09 <102453829+Luckyboyyy09@users.noreply.github.com>
2024-01-18 15:09:50 +0800

    new update for BA models

--- models/BeritaAcara.js
+++ models/BeritaAcara.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Sequelize } from "sequelize";
 import db from "../config/Database.js";
 import Row from "./MonitoringRow.js";
@@ -18,18 +17,6 @@ const Berita_Acara = db.define(
     nomor_tower: {
       type: DataTypes.INTEGER,
     },
-    jumlah_tegakan: {
-      type: DataTypes.INTEGER,
-    },
-    status_tegakan: {
-      type: DataTypes.ENUM("aman", "kritis"),
-    },
-    jenis_pohon: {
-      type: DataTypes.STRING,
-    },
-    jalur: {
-      type: DataTypes.ENUM("bawah", "luar"),
-    },
     tindak_lanjut: {
       type: DataTypes.ENUM("sudah pangkas", "sudah tebang"),
     },
@@ -39,77 +26,14 @@ const Berita_Acara = db.define(
     gambar_setelah: {
       type: DataTypes.STRING,
     },
-  },
-  {
-    freezeTableName: true,
-    // If don't want updatedAt
-    updatedAt: false,
-  }
-);
-
-// Tambahkan relasi dengan model MonitoringRow
-Berita_Acara.hasOne(Row, { foreignKey: "nomor_tower" });
-// Berita_Acara.belongsTo(Row, { foreignKey: "nomor_tower" });
-
-Berita_Acara.hasOne(Users, { foreignKey: "nomor_tower" });
-// Berita_Acara.belongsTo(Users, { foreignKey: "nomor_tower" });
-
-export default Berita_Acara;
-=======
-import { Sequelize } from "sequelize";
-import db from "../config/Database.js";
-import Row from "./MonitoringRow.js";
-import Users from "./UserModel.js";
-
-const { DataTypes } = Sequelize;
-
-const Berita_Acara = db.define(
-  "Berita_Acara",
-  {
-    nama_PIC: {
-      type: DataTypes.STRING,
-    },
-    id_users: {
-      type: DataTypes.INTEGER,
-    },
-    nomor_tower: {
-      type: DataTypes.INTEGER,
-    },
-    jumlah_tegakan: {
-      type: DataTypes.INTEGER,
-    },
-    status_tegakan: {
-      type: DataTypes.ENUM("aman", "kritis"),
-    },
-    jenis_pohon: {
-      type: DataTypes.STRING,
-    },
-    jalur: {
-      type: DataTypes.ENUM("bawah", "luar"),
-    },
-    tindak_lanjut: {
-      type: DataTypes.ENUM("sudah pangkas", "sudah tebang"),
-    },
-    gambar_sebelum: {
-      type: DataTypes.STRING,
-    },
-    gambar_setelah: {
+    rute_transmisi: {
       type: DataTypes.STRING,
     },
   },
   {
     freezeTableName: true,
-    // If don't want updatedAt
     updatedAt: false,
   }
 );
 
-// Tambahkan relasi dengan model MonitoringRow
-Berita_Acara.hasOne(Row, { foreignKey: "nomor_tower" });
-// Berita_Acara.belongsTo(Row, { foreignKey: "nomor_tower" });
-
-Berita_Acara.hasOne(Users, { foreignKey: "nomor_tower" });
-// Berita_Acara.belongsTo(Users, { foreignKey: "nomor_tower" });
-
 export default Berita_Acara;
->>>>>>> origin/main
