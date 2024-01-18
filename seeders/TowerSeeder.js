5041186e0c914f805ce6ff26256a913b02c6f14c
luckyboyyy09 <102453829+Luckyboyyy09@users.noreply.github.com>
2024-01-18 15:16:59 +0800

    new update towerseeder

---
+++ seeders/TowerSeeder.js
@@ -0,0 +1,34 @@
+import Towers from "../models/Tower.js";
+
+Towers.bulkCreate([
+  {
+    name: "PLN GARDU INDUK DAYA",
+    jumlah: 149,
+    lokasi: "DAYA",
+  },
+  {
+    name: "GARDU INDUK 150KV TELLO ",
+    jumlah: 122,
+    lokasi: "TELLO",
+  },
+  {
+    name: "GARDU INDUK PLN V MANDAI",
+    jumlah: 199,
+    lokasi: "MANDAI",
+  },
+  {
+    name: "PLN GARDU INDUK KIMA",
+    jumlah: 120,
+    lokasi: "KIMA",
+  },
+  {
+    name: "PLN GARDU INDUK MAROS",
+    jumlah: 239,
+    lokasi: "MAROS",
+  },
+  {
+    name: "PLN GARDU INDUK BOSOWA",
+    jumlah: 93,
+    lokasi: "BOSOWA",
+  },
+]);
